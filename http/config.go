8e65c3d01d76779fddda061a5127a26361eb3e80
omertirosh <omertirosh3@gmail.com>
2020-11-17 14:20:44 +0200

    Update config.go

--- http/config.go
+++ http/config.go
@@ -7,22 +7,22 @@ import (
 )
 
 type httpConfig struct {
-	Protocol         string               `config:"protocol"`
-	Path             string               `config:"path"`
-	Params           map[string]string    `config:"parameters"`
-	Username         string               `config:"username"`
-	Password         string               `config:"password"`
-	ProxyURL         string               `config:"proxy_url"`
-	LoadBalance      bool                 `config:"loadbalance"`
-	BatchPublish     bool                 `config:"batch_publish"`
-	BatchSize        int                  `config:"batch_size"`
-	CompressionLevel int                  `config:"compression_level" validate:"min=0, max=9"`
+	Protocol         string            `config:"protocol"`
+	Path             string            `config:"path"`
+	Params           map[string]string `config:"parameters"`
+	Username         string            `config:"username"`
+	Password         string            `config:"password"`
+	ProxyURL         string            `config:"proxy_url"`
+	LoadBalance      bool              `config:"loadbalance"`
+	BatchPublish     bool              `config:"batch_publish"`
+	BatchSize        int               `config:"batch_size"`
+	CompressionLevel int               `config:"compression_level" validate:"min=0, max=9"`
 	TLS              *tlscommon.Config `config:"tls"`
-	MaxRetries       int                  `config:"max_retries"`
-	Timeout          time.Duration        `config:"timeout"`
-	Headers          map[string]string    `config:"headers"`
-	ContentType      string               `config:"content_type"`
-	Backoff          backoff              `config:"backoff"`
+	MaxRetries       int               `config:"max_retries"`
+	Timeout          time.Duration     `config:"timeout"`
+	Headers          map[string]string `config:"headers"`
+	ContentType      string            `config:"content_type"`
+	Backoff          backoff           `config:"backoff"`
 }
 
 type backoff struct {
